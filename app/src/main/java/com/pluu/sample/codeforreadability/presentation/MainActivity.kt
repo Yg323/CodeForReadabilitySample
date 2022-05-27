b1a2f9f8d9e7f1faaaf9e5d9aacf3f5cf27e926d
Yoongyeong Song <14dnfnfn@gmail.com>
2022-05-27 12:13:32 +0900

    Step4. move generate item

--- app/src/main/java/com/pluu/sample/codeforreadability/presentation/MainActivity.kt
+++ app/src/main/java/com/pluu/sample/codeforreadability/presentation/MainActivity.kt
@@ -2,13 +2,11 @@ package com.pluu.sample.codeforreadability.presentation
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.pluu.sample.codeforreadability.databinding.ActivityMainBinding
-import com.pluu.sample.codeforreadability.model.SampleItem
 import com.pluu.sample.codeforreadability.utils.dp
 
 class MainActivity : AppCompatActivity() {
@@ -29,6 +27,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setUpViews()
+        setUpObservers()
     }
 
     private fun setUpViews() {
@@ -39,17 +38,14 @@ class MainActivity : AppCompatActivity() {
                 }
                 sampleAdapter.updateFavorite(it)
                 sampleAdapter.notifyDataSetChanged()
-            },
-            onDuplicate = { item ->
-                Toast.makeText(this, "Duplicate item : ${item.text}", Toast.LENGTH_SHORT).show()
             }
         )
 
         binding.btnGenerate.setOnClickListener {
-            generate()
+            viewModel.generate()
         }
         binding.btnClear.setOnClickListener {
-            reset()
+            viewModel.reset()
         }
         with(binding.recyclerView) {
             adapter = sampleAdapter
@@ -59,22 +55,13 @@ class MainActivity : AppCompatActivity() {
         sampleAdapter.updateFavorite(preferences.getString("KEY", null).orEmpty())
     }
 
-    private fun generate() {
-        val item = SampleItem(
-            text = ('a' + (0 until 26).random()).toString(),
-            bgColor = Color.rgb(
-                (0..255).random(),
-                (0..255).random(),
-                (0..255).random()
-            )
-        )
-
-        sampleAdapter.addItem(item)
-    }
-
-    private fun reset() {
-        viewModel.sendLog()
-        sampleAdapter.reset()
-        binding.recyclerView.scrollToPosition(0)
+    private fun setUpObservers() {
+        viewModel.items.observe(this) { list ->
+            sampleAdapter.submitList(list)
+            binding.recyclerView.scrollToPosition(0)
+        }
+        viewModel.messageEvent.observe(this) { msg ->
+            Toast.makeText(this, "Duplicate item : $msg", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/pluu/sample/codeforreadability/presentation/SearchViewModel.kt
+++ app/src/main/java/com/pluu/sample/codeforreadability/presentation/SearchViewModel.kt
@@ -1,7 +1,11 @@
 package com.pluu.sample.codeforreadability.presentation
 
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.pluu.sample.codeforreadability.model.SampleItem
 import com.pluu.sample.codeforreadability.provider.provideRepository
 import kotlinx.coroutines.launch
 import logcat.logcat
@@ -12,7 +16,39 @@ class SearchViewModel : ViewModel() {
         provideRepository()
     }
 
-    fun sendLog() {
+    private val _items = MutableLiveData<List<SampleItem>>()
+    val items: LiveData<List<SampleItem>> get() = _items
+
+    private val _messageEvent = MutableLiveData<String>()
+    val messageEvent: LiveData<String> get() = _messageEvent
+
+    private val cachedList = mutableListOf<SampleItem>()
+
+    fun generate() {
+        val item = SampleItem(
+            text = ('a' + (0 until 26).random()).toString(),
+            bgColor = Color.rgb(
+                (0..255).random(),
+                (0..255).random(),
+                (0..255).random()
+            )
+        )
+
+        if (cachedList.none { it.text == item.text }) {
+            cachedList.add(item)
+            _items.value = cachedList.sortedBy { it.text }
+        } else {
+            _messageEvent.value = "Duplicate item : ${item.text}"
+        }
+    }
+
+    fun reset() {
+        sendLog()
+        cachedList.clear()
+        _items.value = emptyList()
+    }
+
+    private fun sendLog() {
         // Use OkHttp, Retrofit
         viewModelScope.launch {
             // Case 1.
